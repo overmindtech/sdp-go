dc998ebd8a309776288341c1f96905ad1394b5f8
Dylan Ratcliffe <dylanratcliffe@outlook.com>
2022-06-01 18:22:04 +0200

    Made test connection exported

--- progress_test.go
+++ progress_test.go
@@ -2,7 +2,6 @@ package sdp
 
 import (
 	"fmt"
-	"sync"
 	"testing"
 	"time"
 
@@ -10,68 +9,6 @@ import (
 	"github.com/nats-io/nats.go"
 	"google.golang.org/protobuf/types/known/durationpb"
 )
-
-type ResponseMessage struct {
-	Subject string
-	V       interface{}
-}
-
-// TestConnection Used to mock a NATS connection for testing
-type TestConnection struct {
-	Messages           []ResponseMessage
-	Subscriptions      map[string][]nats.Handler
-	messagesMutex      sync.Mutex
-	subscriptionsMutex sync.Mutex
-}
-
-// Publish Test publish method, notes down the subject and the message
-func (t *TestConnection) Publish(subject string, v interface{}) error {
-	t.messagesMutex.Lock()
-	t.Messages = append(t.Messages, ResponseMessage{
-		Subject: subject,
-		V:       v,
-	})
-	t.messagesMutex.Unlock()
-	return nil
-}
-
-func (t *TestConnection) Subscribe(subject string, cb nats.Handler) (*nats.Subscription, error) {
-	t.subscriptionsMutex.Lock()
-	defer t.subscriptionsMutex.Unlock()
-
-	if t.Subscriptions == nil {
-		t.Subscriptions = make(map[string][]nats.Handler)
-	}
-
-	t.Subscriptions[subject] = append(t.Subscriptions[subject], cb)
-
-	return nil, nil
-}
-
-// SendMessage Emulates a message being sent on a particular subject. The object
-// passed should be the decoded content of the message i.e. the Item not the
-// binary representation
-func (t *TestConnection) SendMessage(subject string, object interface{}) {
-	t.subscriptionsMutex.Lock()
-	defer t.subscriptionsMutex.Unlock()
-
-	handlers, ok := t.Subscriptions[subject]
-
-	if ok {
-		for _, handler := range handlers {
-			switch v := handler.(type) {
-			case func(*Item):
-				i := object.(*Item)
-				go v(i)
-			case func(*Response):
-				r := object.(*Response)
-				go v(r)
-			default:
-				panic("unknown handler type")
-			}
-		}
-	}
-}
 
 func TestResponseNilPublisher(t *testing.T) {
 	rs := ResponseSender{
