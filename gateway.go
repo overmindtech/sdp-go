25b16b555dceaacca79e936b034623dca65495e2
Dylan Ratcliffe <dylanratcliffe@outlook.com>
2022-06-22 14:57:04 +0000

    Added equality check method

---
+++ gateway.go
@@ -0,0 +1,68 @@
+package sdp
+
+//Equal Returns whether to statuses are functionally equal
+func (x *GatewayRequestStatus) Equal(y *GatewayRequestStatus) bool {
+	// Check the basics first
+	if len(x.ResponderErrors) != len(y.ResponderErrors) {
+		return false
+	}
+
+	if len(x.ResponderStates) != len(y.ResponderStates) {
+		return false
+	}
+
+	if (x.Summary == nil || y.Summary == nil) && x.Summary != y.Summary {
+		// If one of them is nil, and they aren't both nil
+		return false
+	}
+
+	for xResponder, xErr := range x.ResponderErrors {
+		xErrString := xErr.Error()
+
+		yErr, exists := y.ResponderErrors[xResponder]
+
+		if !exists {
+			return false
+		}
+
+		if yErr.Error() != xErrString {
+			return false
+		}
+	}
+
+	for xResponder, xState := range x.ResponderStates {
+		yState, exists := y.ResponderStates[xResponder]
+
+		if !exists {
+			return false
+		}
+
+		if yState != xState {
+			return false
+		}
+	}
+
+	if x.Summary != nil && y.Summary != nil {
+		if x.Summary.Working != y.Summary.Working {
+			return false
+		}
+		if x.Summary.Stalled != y.Summary.Stalled {
+			return false
+		}
+		if x.Summary.Complete != y.Summary.Complete {
+			return false
+		}
+		if x.Summary.Failed != y.Summary.Failed {
+			return false
+		}
+		if x.Summary.Cancelled != y.Summary.Cancelled {
+			return false
+		}
+		if x.Summary.Responders != y.Summary.Responders {
+			return false
+		}
+
+	}
+
+	return true
+}
---
+++ gateway_test.go
@@ -0,0 +1,205 @@
+package sdp
+
+import "testing"
+
+func TestEqual(t *testing.T) {
+	x := &GatewayRequestStatus{
+		ResponderStates: map[string]ResponderState{
+			"foo": ResponderState_COMPLETE,
+			"boo": ResponderState_WORKING,
+			"bar": ResponderState_ERROR,
+		},
+		ResponderErrors: map[string]*ItemRequestError{
+			"bar": {
+				ErrorType:   ItemRequestError_TIMEOUT,
+				Context:     "test",
+				ErrorString: "timed out",
+			},
+		},
+		Summary: &GatewayRequestStatus_Summary{
+			Working:    1,
+			Stalled:    0,
+			Complete:   1,
+			Failed:     1,
+			Cancelled:  0,
+			Responders: 3,
+		},
+	}
+
+	t.Run("with nil summary", func(t *testing.T) {
+		y := &GatewayRequestStatus{
+			ResponderStates: map[string]ResponderState{
+				"foo": ResponderState_COMPLETE,
+				"boo": ResponderState_WORKING,
+				"bar": ResponderState_ERROR,
+			},
+			ResponderErrors: map[string]*ItemRequestError{
+				"bar": {
+					ErrorType:   ItemRequestError_TIMEOUT,
+					Context:     "test",
+					ErrorString: "timed out",
+				},
+			},
+		}
+
+		if x.Equal(y) {
+			t.Error("expected items to be nonequal")
+		}
+	})
+
+	t.Run("with nil ResponderErrors", func(t *testing.T) {
+		y := &GatewayRequestStatus{
+			ResponderStates: map[string]ResponderState{
+				"foo": ResponderState_COMPLETE,
+				"boo": ResponderState_WORKING,
+				"bar": ResponderState_ERROR,
+			},
+			Summary: &GatewayRequestStatus_Summary{
+				Working:    1,
+				Stalled:    0,
+				Complete:   1,
+				Failed:     1,
+				Cancelled:  0,
+				Responders: 3,
+			},
+		}
+
+		if x.Equal(y) {
+			t.Error("expected items to be nonequal")
+		}
+	})
+
+	t.Run("with nil ResponderStates", func(t *testing.T) {
+		y := &GatewayRequestStatus{
+			ResponderErrors: map[string]*ItemRequestError{
+				"bar": {
+					ErrorType:   ItemRequestError_TIMEOUT,
+					Context:     "test",
+					ErrorString: "timed out",
+				},
+			},
+			Summary: &GatewayRequestStatus_Summary{
+				Working:    1,
+				Stalled:    0,
+				Complete:   1,
+				Failed:     1,
+				Cancelled:  0,
+				Responders: 3,
+			},
+		}
+
+		if x.Equal(y) {
+			t.Error("expected items to be nonequal")
+		}
+	})
+
+	t.Run("with mismatched summary", func(t *testing.T) {
+		y := &GatewayRequestStatus{
+			ResponderStates: map[string]ResponderState{
+				"foo": ResponderState_COMPLETE,
+				"boo": ResponderState_WORKING,
+				"bar": ResponderState_ERROR,
+			},
+			ResponderErrors: map[string]*ItemRequestError{
+				"bar": {
+					ErrorType:   ItemRequestError_TIMEOUT,
+					Context:     "test",
+					ErrorString: "timed out",
+				},
+			},
+			Summary: &GatewayRequestStatus_Summary{
+				Working:    1,
+				Stalled:    0,
+				Complete:   3,
+				Failed:     1,
+				Cancelled:  0,
+				Responders: 3,
+			},
+		}
+
+		if x.Equal(y) {
+			t.Error("expected items to be nonequal")
+		}
+	})
+
+	t.Run("with mismatched ResponderErrors", func(t *testing.T) {
+		y := &GatewayRequestStatus{
+			ResponderStates: map[string]ResponderState{
+				"foo": ResponderState_COMPLETE,
+				"boo": ResponderState_WORKING,
+				"bar": ResponderState_ERROR,
+			},
+			ResponderErrors: map[string]*ItemRequestError{},
+			Summary: &GatewayRequestStatus_Summary{
+				Working:    1,
+				Stalled:    0,
+				Complete:   1,
+				Failed:     1,
+				Cancelled:  0,
+				Responders: 3,
+			},
+		}
+
+		if x.Equal(y) {
+			t.Error("expected items to be nonequal")
+		}
+	})
+
+	t.Run("with mismatched ResponderStates", func(t *testing.T) {
+		y := &GatewayRequestStatus{
+			ResponderStates: map[string]ResponderState{
+				"foo": ResponderState_COMPLETE,
+				"BOO": ResponderState_WORKING,
+				"bar": ResponderState_ERROR,
+			},
+			ResponderErrors: map[string]*ItemRequestError{
+				"bar": {
+					ErrorType:   ItemRequestError_TIMEOUT,
+					Context:     "test",
+					ErrorString: "timed out",
+				},
+			},
+			Summary: &GatewayRequestStatus_Summary{
+				Working:    1,
+				Stalled:    0,
+				Complete:   1,
+				Failed:     1,
+				Cancelled:  0,
+				Responders: 3,
+			},
+		}
+
+		if x.Equal(y) {
+			t.Error("expected items to be nonequal")
+		}
+	})
+
+	t.Run("with same everything", func(t *testing.T) {
+		y := &GatewayRequestStatus{
+			ResponderStates: map[string]ResponderState{
+				"foo": ResponderState_COMPLETE,
+				"boo": ResponderState_WORKING,
+				"bar": ResponderState_ERROR,
+			},
+			ResponderErrors: map[string]*ItemRequestError{
+				"bar": {
+					ErrorType:   ItemRequestError_TIMEOUT,
+					Context:     "test",
+					ErrorString: "timed out",
+				},
+			},
+			Summary: &GatewayRequestStatus_Summary{
+				Working:    1,
+				Stalled:    0,
+				Complete:   1,
+				Failed:     1,
+				Cancelled:  0,
+				Responders: 3,
+			},
+		}
+
+		if !x.Equal(y) {
+			t.Error("expected items to be equal")
+		}
+	})
+}
