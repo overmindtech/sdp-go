b6226d90127cbb6c7ad27dd919d196dfdd34ef04
Engineering <github-action@users.noreply.github.com>
2023-07-25 08:58:49 +0000

    Updated to SDP v0.18.4-103-ga552669

--- sdpconnect/snapshots.connect.go
+++ sdpconnect/snapshots.connect.go
@@ -48,6 +48,9 @@ const (
 	// SnapshotsServiceDeleteSnapshotProcedure is the fully-qualified name of the SnapshotsService's
 	// DeleteSnapshot RPC.
 	SnapshotsServiceDeleteSnapshotProcedure = "/snapshots.SnapshotsService/DeleteSnapshot"
+	// SnapshotsServiceGetInitialDataProcedure is the fully-qualified name of the SnapshotsService's
+	// GetInitialData RPC.
+	SnapshotsServiceGetInitialDataProcedure = "/snapshots.SnapshotsService/GetInitialData"
 )
 
 // SnapshotsServiceClient is a client for the snapshots.SnapshotsService service.
@@ -57,6 +60,8 @@ type SnapshotsServiceClient interface {
 	GetSnapshot(context.Context, *connect_go.Request[sdp_go.GetSnapshotRequest]) (*connect_go.Response[sdp_go.GetSnapshotResponse], error)
 	UpdateSnapshot(context.Context, *connect_go.Request[sdp_go.UpdateSnapshotRequest]) (*connect_go.Response[sdp_go.UpdateSnapshotResponse], error)
 	DeleteSnapshot(context.Context, *connect_go.Request[sdp_go.DeleteSnapshotRequest]) (*connect_go.Response[sdp_go.DeleteSnapshotResponse], error)
+	// retrieve the initial data for the example change
+	GetInitialData(context.Context, *connect_go.Request[sdp_go.GetInitialDataRequest]) (*connect_go.Response[sdp_go.GetInitialDataResponse], error)
 }
 
 // NewSnapshotsServiceClient constructs a client for the snapshots.SnapshotsService service. By
@@ -94,6 +99,11 @@ func NewSnapshotsServiceClient(httpClient connect_go.HTTPClient, baseURL string,
 			baseURL+SnapshotsServiceDeleteSnapshotProcedure,
 			opts...,
 		),
+		getInitialData: connect_go.NewClient[sdp_go.GetInitialDataRequest, sdp_go.GetInitialDataResponse](
+			httpClient,
+			baseURL+SnapshotsServiceGetInitialDataProcedure,
+			opts...,
+		),
 	}
 }
 
@@ -104,6 +114,7 @@ type snapshotsServiceClient struct {
 	getSnapshot    *connect_go.Client[sdp_go.GetSnapshotRequest, sdp_go.GetSnapshotResponse]
 	updateSnapshot *connect_go.Client[sdp_go.UpdateSnapshotRequest, sdp_go.UpdateSnapshotResponse]
 	deleteSnapshot *connect_go.Client[sdp_go.DeleteSnapshotRequest, sdp_go.DeleteSnapshotResponse]
+	getInitialData *connect_go.Client[sdp_go.GetInitialDataRequest, sdp_go.GetInitialDataResponse]
 }
 
 // ListSnapshots calls snapshots.SnapshotsService.ListSnapshots.
@@ -131,6 +142,11 @@ func (c *snapshotsServiceClient) DeleteSnapshot(ctx context.Context, req *connec
 	return c.deleteSnapshot.CallUnary(ctx, req)
 }
 
+// GetInitialData calls snapshots.SnapshotsService.GetInitialData.
+func (c *snapshotsServiceClient) GetInitialData(ctx context.Context, req *connect_go.Request[sdp_go.GetInitialDataRequest]) (*connect_go.Response[sdp_go.GetInitialDataResponse], error) {
+	return c.getInitialData.CallUnary(ctx, req)
+}
+
 // SnapshotsServiceHandler is an implementation of the snapshots.SnapshotsService service.
 type SnapshotsServiceHandler interface {
 	ListSnapshots(context.Context, *connect_go.Request[sdp_go.ListSnapshotsRequest]) (*connect_go.Response[sdp_go.ListSnapshotResponse], error)
@@ -138,6 +154,8 @@ type SnapshotsServiceHandler interface {
 	GetSnapshot(context.Context, *connect_go.Request[sdp_go.GetSnapshotRequest]) (*connect_go.Response[sdp_go.GetSnapshotResponse], error)
 	UpdateSnapshot(context.Context, *connect_go.Request[sdp_go.UpdateSnapshotRequest]) (*connect_go.Response[sdp_go.UpdateSnapshotResponse], error)
 	DeleteSnapshot(context.Context, *connect_go.Request[sdp_go.DeleteSnapshotRequest]) (*connect_go.Response[sdp_go.DeleteSnapshotResponse], error)
+	// retrieve the initial data for the example change
+	GetInitialData(context.Context, *connect_go.Request[sdp_go.GetInitialDataRequest]) (*connect_go.Response[sdp_go.GetInitialDataResponse], error)
 }
 
 // NewSnapshotsServiceHandler builds an HTTP handler from the service implementation. It returns the
@@ -171,6 +189,11 @@ func NewSnapshotsServiceHandler(svc SnapshotsServiceHandler, opts ...connect_go.
 		svc.DeleteSnapshot,
 		opts...,
 	)
+	snapshotsServiceGetInitialDataHandler := connect_go.NewUnaryHandler(
+		SnapshotsServiceGetInitialDataProcedure,
+		svc.GetInitialData,
+		opts...,
+	)
 	return "/snapshots.SnapshotsService/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		switch r.URL.Path {
 		case SnapshotsServiceListSnapshotsProcedure:
@@ -183,6 +206,8 @@ func NewSnapshotsServiceHandler(svc SnapshotsServiceHandler, opts ...connect_go.
 			snapshotsServiceUpdateSnapshotHandler.ServeHTTP(w, r)
 		case SnapshotsServiceDeleteSnapshotProcedure:
 			snapshotsServiceDeleteSnapshotHandler.ServeHTTP(w, r)
+		case SnapshotsServiceGetInitialDataProcedure:
+			snapshotsServiceGetInitialDataHandler.ServeHTTP(w, r)
 		default:
 			http.NotFound(w, r)
 		}
@@ -210,4 +235,8 @@ func (UnimplementedSnapshotsServiceHandler) UpdateSnapshot(context.Context, *con
 
 func (UnimplementedSnapshotsServiceHandler) DeleteSnapshot(context.Context, *connect_go.Request[sdp_go.DeleteSnapshotRequest]) (*connect_go.Response[sdp_go.DeleteSnapshotResponse], error) {
 	return nil, connect_go.NewError(connect_go.CodeUnimplemented, errors.New("snapshots.SnapshotsService.DeleteSnapshot is not implemented"))
+}
+
+func (UnimplementedSnapshotsServiceHandler) GetInitialData(context.Context, *connect_go.Request[sdp_go.GetInitialDataRequest]) (*connect_go.Response[sdp_go.GetInitialDataResponse], error) {
+	return nil, connect_go.NewError(connect_go.CodeUnimplemented, errors.New("snapshots.SnapshotsService.GetInitialData is not implemented"))
 }
