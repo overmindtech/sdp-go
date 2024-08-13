e86eaa290346a655623dbfbb5a97bf1c6dcca8a2
Dylan Ratcliffe <dylan@overmind.tech>
2024-08-13 11:28:42 +0000

    Added defaults

--- util.go
+++ util.go
@@ -5,14 +5,29 @@ import "math"
 // CalculatePaginationOffsetLimit Calculates the offset and limit for pagination
 // in SQL queries, along with the current page and total pages that should be
 // included in the response
+//
+// This also sets sane defaults for the page size if pagination is not provided.
+// These defaults are page 1 with a page size of 10
 func CalculatePaginationOffsetLimit(pagination *PaginationRequest, totalItems int32) (offset, limit, page, totalPages int32) {
+	var requestedPageSize int32
+	var requestedPage int32
+
+	if pagination == nil {
+		// Set sane defaults
+		requestedPageSize = 10
+		requestedPage = 1
+	} else {
+		requestedPageSize = pagination.GetPageSize()
+		requestedPage = pagination.GetPage()
+	}
+
 	// pagesize is at least 10, at most 100
-	limit = min(100, max(10, pagination.GetPageSize()))
+	limit = min(100, max(10, requestedPageSize))
 	// calculate the total number of pages
 	totalPages = int32(math.Ceil(float64(totalItems) / float64(limit)))
 
 	// page has to be at least 1, and at most totalPages
-	page = min(totalPages, pagination.GetPage())
+	page = min(totalPages, requestedPage)
 	page = max(1, page)
 
 	// calculate the offset
