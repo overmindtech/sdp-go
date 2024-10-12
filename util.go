e7689fc74aa89508b1db7d8c3f26e477ef6086d7
Dylan Ratcliffe <dylan@overmind.tech>
2024-10-12 12:11:35 +0000

    Added a shared type that can be used to export metadata

--- util.go
+++ util.go
@@ -45,3 +45,33 @@ func CalculatePaginationOffsetLimit(pagination *PaginationRequest, totalItems in
 	}
 	return offset, limit, page, totalPages
 }
+
+// An object that returns all of the adapter metadata for a given source
+type AdapterMetadataProvider interface {
+	AllAdapterMetadata() []*AdapterMetadata
+}
+
+// A list of adapter metadata, this is used to store all the adapter metadata
+// for a given source so that it can be retrieved later for the purposes of
+// generating documentation and Terraform mappings
+type AdapterMetadataList struct {
+	// The list of adapter metadata
+	list []*AdapterMetadata
+}
+
+// AllAdapterMetadata returns all the adapter metadata
+func (a *AdapterMetadataList) AllAdapterMetadata() []*AdapterMetadata {
+	return a.list
+}
+
+// RegisterAdapterMetadata registers a new adapter metadata with the list and
+// returns a pointer to that same metadata to be used elsewhere
+func (a *AdapterMetadataList) Register(metadata *AdapterMetadata) *AdapterMetadata {
+	if a == nil {
+		return metadata
+	}
+
+	a.list = append(a.list, metadata)
+
+	return metadata
+}
